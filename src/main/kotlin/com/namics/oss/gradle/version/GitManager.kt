c7b3f420633786c938c518087f9d48eb05676b19
aschaefer <andre.schaefer@namics.com>
2019-04-30 13:13:44 +0200

    fix: recursion issue

--- src/main/kotlin/com/namics/oss/gradle/version/GitManager.kt
+++ src/main/kotlin/com/namics/oss/gradle/version/GitManager.kt
@@ -46,7 +46,7 @@ class GitManager(private val project: Project) {
             .map { it.replace(Regex("""\*|\s+"""), "") }
             .get()
 
-    fun status(): Stream<String> = git("branch", "status")
+    fun status(): Stream<String> = git("status")
 
     fun add(file: File): Stream<String> = git("add", file.path)
 
@@ -69,13 +69,13 @@ class GitManager(private val project: Project) {
 
     fun git(vararg arguments: String): Stream<String> {
         logger.info("GIT: On branch ${branch()}")
+        logger.info("GIT: git {}", arguments)
         status().forEach{logger.info("GIT: $it")}
         val output = perform(*arguments)
         return output.asSequence().onEach { logger.info("GIT: $it") }.asStream()
     }
 
     private fun perform(vararg arguments: String): Stream<String> {
-        logger.info("GIT: git {}", arguments)
         try {
             val process = ProcessBuilder("git", *arguments)
                     .directory(root)
